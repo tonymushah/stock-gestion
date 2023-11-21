7dc17acc9798abc3d8f7e8c1c0d647238e488cde
tonymushah <tonyramanantsalama@gmail.com>
2023-11-21 08:47:36 +0300

    first commit

---
+++ src/main/java/mg/tonymushah/itu/stockgestion/StockGestionApplication.java
@@ -0,0 +1,13 @@
+package mg.tonymushah.itu.stockgestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockGestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockGestionApplication.class, args);
+	}
+
+}
